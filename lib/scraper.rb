5b73e149e0b32c5cf921863abbb61eb6c3659d8a
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-18 20:29:25 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,12 @@ class Scraper
     html = open('http://learn-co-curriculum.github.io/site-for-scraping/courses')
     Nokogiri::HTML(html)
     # binding.pry
-
+    # doc.css(".post").each do |post|
+    #   course = Course.new
+    #   course.title = post.css("h2").text
+    #   course.schedule = post.css(".date").text
+    #   course.description = post.css("p").text
+    # end
   end
 
   def get_courses
